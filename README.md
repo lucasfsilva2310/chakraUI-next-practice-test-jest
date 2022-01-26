e1784b09655091ac69b88343ae7a9e30bc4df5b7
lucas <lucasfsilva2310@gmail.com>
2022-01-26 12:10:17 -0300

    creating Sidebar component and adding Chat library as well as dynamic from next to use charts on our app

--- README.md
+++ README.md
@@ -27,3 +27,15 @@ Another cool thing is: if we want to still use a Flex component, but we need tha
 
 Typescript Types
 To create a new component, we need to declare a interface, a contract, to what possible props that component could use, we can take for example the input component as well. By creating a InputProps, we declared all new types that we want to use, but we still need to declare alll default props that the input element needs to use, a possible solution for that is extending our interface, for it to have all of input props as well as our new ones
+
+React-Icon library
+this library is simple , it works simply as a icon library, using it with chakra UI we can include it by the same method with "as" inside a component, but instead of adapting to a new html element, we adapt to receive the icon, importing the Icon component from chakra
+
+ApexCharts - charts library
+to import this library just run 'yarn add apexcharts react-apexcharts'
+this library is an easy way to represent data as charts, you just have to use like a normal react component
+theres two importante properties that needs to bem filled:
+options -
+series - type of data that will be represented, and the data itself, like : { name: 'series', data: [1,4,6]}
+OBS: when we´re using next.js, we need to find a way to just use this library inside the browser, as we know next run a sort of back end node server when its rendering the basic html, so if the browser window does not exist yet, this library will return an error similar to "window not defined". for that, we can use dinamyc, from next/dinamyc, and we import Chart using this import, for example "const Chart = dynamic(() => import('react-apexcharts'), {ssr: false})"
+So from now on, this library will only be imported later on (the ssr(server-side-rendering) value ensure that this lib will not be used on the server side)
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,84 @@
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+import dinamyc from "next/dynamic";
 
+const Chart = dinamyc(() => import("react-apexcharts"), {
+  ssr: false,
+});
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2022-01-26T00:00:00.000Z",
+      "2022-01-27T00:00:00.000Z",
+      "2022-01-28T00:00:00.000Z",
+      "2022-01-29T00:00:00.000Z",
+      "2022-01-30T00:00:00.000Z",
+      "2022-01-31T00:00:00.000Z",
+      "2022-02-01T00:00:00.000Z",
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const series = [{ name: "series1", data: [1, 123, 20, 50, 240, 70, 28] }];
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex=start">
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  );
 }
